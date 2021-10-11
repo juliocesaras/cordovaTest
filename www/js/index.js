bd678ff2f24af478bc574643520c60b98e2e8d6e
Julio Cesar Araya Soto <juliocesaras@gmail.com>
2021-10-11 12:24:01 -0600

    primera version

---
+++ www/js/index.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI });
+
+}
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
